653bcdea1df17b767e78d2615353ca2dae917e07
nuraini300896 <45997913+nuraini300896@users.noreply.github.com>
2018-12-26 16:04:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 echo "
 <html>
 <body>
-<center><a style='margin-top:200px;backgroud-color:pink;'>215 280 045
+<center>
+<table style='margin-top:200px;backgroud-color:pink;'>
+<tr>
+<td>215 280 045</td>
+</tr>
+<tr>
+<td>Nuraini</td>
+</tr>
+</table>
 </body>
 </html>
 ";
-
-echo "<br> Nuraini</a>";
 ?>
